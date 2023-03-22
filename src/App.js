a821637e02103e103bcf9797a65370320b8b66c9
Bárbara Soares <bahsoares@live.com>
2023-03-22 20:08:50 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-
+import { FiSearch } from 'react-icons/fi'
 function App() {
   return (
     <div className="Conteiner">
-      <h1 className="Title"> Buscador de CEP</h1>
+      <h1 className="Title"> 
+      Buscador de CEP</h1>
 
       <div className="ConteinerInput">
       <input type="text"
       placeholder="Digite seu CEP..."
       />
+
+    <button className="buttonSearch">
+    <FiSearch 
+    size={25}
+    color="#FFF"/>
+    </button>
+
       </div>
+
+      <main className="main">
+      <h2>CEP:12345678</h2>
+      <span>RUA: qualquer</span>    
+
+      </main>
+
     </div>
   );
 }
